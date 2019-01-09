0103132a733735a199d4b774935d91752d5210f0
Konrad Siek <konrad.siek@gmail.com>
2019-01-09 13:01:49 -0500

    A more specific use of namespaces.

--- src/commit-order.cpp
+++ src/commit-order.cpp
@@ -2,193 +2,194 @@
 
 #include <algorithm>
 
-using namespace dejavu;
+namespace dejavu {
 
-CommitOrder::CommitOrder(std::string const & input_path,
-                         std::string const & output_path,
-                         std::unordered_map<unsigned int, unsigned long> const & timestamps)
-        : input_path(input_path), 
-          output_path(output_path),
-          timestamps(timestamps) {} 
+    CommitOrder::CommitOrder(std::string const &input_path,
+                             std::string const &output_path,
+                             std::unordered_map<unsigned int, unsigned long> const &timestamps)
+            : input_path(input_path),
+              output_path(output_path),
+              timestamps(timestamps) {}
 
-void CommitOrder::read() {
-    first_row = true;
+    void CommitOrder::read() {
+        first_row = true;
 
-    std::ofstream csv_file (output_path, std::ios::out | std::ios::trunc);
-    csv_file.close();
+        std::ofstream csv_file(output_path, std::ios::out | std::ios::trunc);
+        csv_file.close();
 
-    parse(input_path);
-}
-
-unsigned long CommitOrder::getTimestamp(unsigned int commit_id) {
-
-    auto pair = timestamps.find(commit_id);
-    if (pair == timestamps.end()) {
-        std::cerr << "[BELGIUM] Did not find commit #" << commit_id
-                  << " in commits." << std::endl;
-        return 0; // FIXME
+        parse(input_path);
     }
 
-    return pair->second;
-}
+    unsigned long CommitOrder::getTimestamp(unsigned int commit_id) {
 
-CommitInfo const & CommitOrder::getCommit(unsigned int commit_id,
-                                          unsigned int project_id,
-                                          unsigned long timestamp) {
+        auto pair = timestamps.find(commit_id);
+        if (pair == timestamps.end()) {
+            std::cerr << "[BELGIUM] Did not find commit #" << commit_id
+                      << " in commits." << std::endl;
+            return 0; // FIXME
+        }
 
-    auto pair = commits.find(commit_id);
-    if (pair != commits.end()) {
         return pair->second;
     }
 
-    CommitInfo commit;
-    commit.project_id = project_id;
-    commit.commit_id = commit_id;
-    commit.timestamp = timestamp;
+    CommitInfo const &CommitOrder::getCommit(unsigned int commit_id,
+                                             unsigned int project_id,
+                                             unsigned long timestamp) {
 
-    commits[commit_id] = commit;
-    
-    return commits[commit_id];
-}
+        auto pair = commits.find(commit_id);
+        if (pair != commits.end()) {
+            return pair->second;
+        }
 
-void CommitOrder::aggregateProjectInfo(unsigned int project_id,
-                                         unsigned int path_id,
-                                         unsigned int commit_id) {
-    // First, we find the commit associated with this particular row.
-    unsigned long timestamp = getTimestamp(commit_id);
+        CommitInfo commit;
+        commit.project_id = project_id;
+        commit.commit_id = commit_id;
+        commit.timestamp = timestamp;
 
-    // Second, we check if we already have an aggregated commit associated
-    // with this commit id. If we do not we need to create a new
-    // aggregated_commit entry. Otherwise, we grab a reference to it.
-    CommitInfo commit = getCommit(commit_id, project_id, timestamp);
+        commits[commit_id] = commit;
 
-    // Third, we add the file into the aggregated commit's file list.
-    commit.path_ids.insert(path_id);
-}
-
-std::unordered_set<unsigned int> getCommonFiles(CommitInfo & a, CommitInfo & b) {
-    std::unordered_set<unsigned int> intersection;
-    set_intersection(a.path_ids.begin(), a.path_ids.end(),
-                     b.path_ids.begin(), b.path_ids.end(),
-                     inserter(intersection, intersection.begin()));
-    return intersection;
-}
-
-void writeOutToCSV(std::ofstream * csv_file, unsigned int project,
-                   unsigned int predecessor, unsigned int successor,
-                   std::unordered_set<unsigned int> * common_files) {
-
-
-    std::unordered_set<unsigned int>::iterator i;
-    for (i = common_files->begin(); i != common_files->end(); ++i) {
-        unsigned int path_id = *i;
-        (*csv_file) << project << "," 
-                    << predecessor << "," 
-                    << successor << ","
-                    << path_id 
-                    << std::endl;
+        return commits[commit_id];
     }
-}
 
-void CommitOrder::processExistingData() {
+    void CommitOrder::aggregateProjectInfo(unsigned int project_id,
+                                           unsigned int path_id,
+                                           unsigned int commit_id) {
+        // First, we find the commit associated with this particular row.
+        unsigned long timestamp = getTimestamp(commit_id);
 
-    // Open the output file for appending.
-    std::ofstream csv_file;
-    csv_file.open(output_path, std::ios::out | std::ios::app);
+        // Second, we check if we already have an aggregated commit associated
+        // with this commit id. If we do not we need to create a new
+        // aggregated_commit entry. Otherwise, we grab a reference to it.
+        CommitInfo commit = getCommit(commit_id, project_id, timestamp);
 
-    // Let's count relations.
-    int relations = 0;
+        // Third, we add the file into the aggregated commit's file list.
+        commit.path_ids.insert(path_id);
+    }
 
-    // Create the order. Compare all commits within the project amongs each
-    // other.
-    for (auto & outer : commits) {
-        CommitInfo out = outer.second;
+    std::unordered_set<unsigned int> getCommonFiles(CommitInfo &a, CommitInfo &b) {
+        std::unordered_set<unsigned int> intersection;
+        set_intersection(a.path_ids.begin(), a.path_ids.end(),
+                         b.path_ids.begin(), b.path_ids.end(),
+                         inserter(intersection, intersection.begin()));
+        return intersection;
+    }
 
-        for (auto & inner : commits) {
-            CommitInfo in = inner.second;
+    void writeOutToCSV(std::ofstream *csv_file, unsigned int project,
+                       unsigned int predecessor, unsigned int successor,
+                       std::unordered_set<unsigned int> *common_files) {
 
-            // The commit neither precedes nor succeeds itself, the order
-            // relation is not reflexive.
-            if (out.commit_id == in.commit_id)
-                continue;
 
-            // Check if the commits have any files in common. If they do not,
-            // they are not ordered.
-            std::unordered_set<unsigned int> common_files = getCommonFiles(in, out);
-            if (common_files.size() == 0)
-                continue;
-
-            // Retrieve timestamps from the map gathered earlier.
-            unsigned long in_timestamp = getTimestamp(in.commit_id);
-            unsigned long out_timestamp = getTimestamp(out.commit_id);
-
-            // If the timestamps are the same, there is no order relation. I
-            // guess this probably doesn't happen very often, if at all.
-            if (in_timestamp == out_timestamp)
-                continue;
-            
-            // If the in timestamp precedes the out timestamp, we create that
-            // relationship. We output it to a CSV file.
-            if (in_timestamp < out_timestamp) 
-                writeOutToCSV(&csv_file, current_project,
-                              in.commit_id, out.commit_id,
-                              &common_files);
-
-            // If the in timestamp follows the out timestamp, we create that
-            // relationship. We output it to a CSV file.
-            if (in_timestamp > out_timestamp)
-                writeOutToCSV(&csv_file, current_project,
-                              out.commit_id, in.commit_id,
-                              &common_files);
-
-            // Count relations.
-            relations += common_files.size();
+        std::unordered_set<unsigned int>::iterator i;
+        for (i = common_files->begin(); i != common_files->end(); ++i) {
+            unsigned int path_id = *i;
+            (*csv_file) << project << ","
+                        << predecessor << ","
+                        << successor << ","
+                        << path_id
+                        << std::endl;
         }
     }
 
-    csv_file.close();
+    void CommitOrder::processExistingData() {
 
-    std::cerr << "Written out " << relations << " relations for project #"
-              << current_project << std::endl;
+        // Open the output file for appending.
+        std::ofstream csv_file;
+        csv_file.open(output_path, std::ios::out | std::ios::app);
 
-    // Finally, remove all data aggregated so far.
-    //for (auto & iterator : commits) {
-    //    free(iterator.second);
-    //}
-    commits.clear();
-}
+        // Let's count relations.
+        int relations = 0;
 
-void CommitOrder::row(std::vector<std::string> & row){
+        // Create the order. Compare all commits within the project amongs each
+        // other.
+        for (auto &outer : commits) {
+            CommitInfo out = outer.second;
 
-    // Get the basic data from the row, convert into appropriate types.
-    unsigned int project_id = atoi(row[0].c_str());
-    unsigned int path_id = atoi(row[1].c_str());
-    //hash_id_t hash_id = atoi(row[2].c_str());
-    unsigned int commit_id = atoi(row[3].c_str());
+            for (auto &inner : commits) {
+                CommitInfo in = inner.second;
 
-    // If this is the first row, initialize the current project field.
-    if (first_row) {
-        current_project = project_id;
-        first_row = false;
+                // The commit neither precedes nor succeeds itself, the order
+                // relation is not reflexive.
+                if (out.commit_id == in.commit_id)
+                    continue;
 
-        std::cerr << "Reading project #" << project_id << std::endl;
+                // Check if the commits have any files in common. If they do not,
+                // they are not ordered.
+                std::unordered_set<unsigned int> common_files = getCommonFiles(in, out);
+                if (common_files.size() == 0)
+                    continue;
+
+                // Retrieve timestamps from the map gathered earlier.
+                unsigned long in_timestamp = getTimestamp(in.commit_id);
+                unsigned long out_timestamp = getTimestamp(out.commit_id);
+
+                // If the timestamps are the same, there is no order relation. I
+                // guess this probably doesn't happen very often, if at all.
+                if (in_timestamp == out_timestamp)
+                    continue;
+
+                // If the in timestamp precedes the out timestamp, we create that
+                // relationship. We output it to a CSV file.
+                if (in_timestamp < out_timestamp)
+                    writeOutToCSV(&csv_file, current_project,
+                                  in.commit_id, out.commit_id,
+                                  &common_files);
+
+                // If the in timestamp follows the out timestamp, we create that
+                // relationship. We output it to a CSV file.
+                if (in_timestamp > out_timestamp)
+                    writeOutToCSV(&csv_file, current_project,
+                                  out.commit_id, in.commit_id,
+                                  &common_files);
+
+                // Count relations.
+                relations += common_files.size();
+            }
+        }
+
+        csv_file.close();
+
+        std::cerr << "Written out " << relations << " relations for project #"
+                  << current_project << std::endl;
+
+        // Finally, remove all data aggregated so far.
+        //for (auto & iterator : commits) {
+        //    free(iterator.second);
+        //}
+        commits.clear();
     }
 
-    // If the project changed, process all the data collected so far and
-    // initialize a new project.
-    if (project_id != current_project) {
-        std::cerr << "Done reading project #" << current_project << std::endl;
-        std::cerr << "Read " << commits.size() << " commits" << std::endl;
+    void CommitOrder::row(std::vector<std::string> &row) {
 
-        std::cerr << "Processing commits for project #" << current_project
-                  << std::endl;
+        // Get the basic data from the row, convert into appropriate types.
+        unsigned int project_id = atoi(row[0].c_str());
+        unsigned int path_id = atoi(row[1].c_str());
+        //hash_id_t hash_id = atoi(row[2].c_str());
+        unsigned int commit_id = atoi(row[3].c_str());
 
-        processExistingData();
-        current_project = project_id;
+        // If this is the first row, initialize the current project field.
+        if (first_row) {
+            current_project = project_id;
+            first_row = false;
 
-        std::cerr << "Reading project #" << project_id << std::endl;
+            std::cerr << "Reading project #" << project_id << std::endl;
+        }
+
+        // If the project changed, process all the data collected so far and
+        // initialize a new project.
+        if (project_id != current_project) {
+            std::cerr << "Done reading project #" << current_project << std::endl;
+            std::cerr << "Read " << commits.size() << " commits" << std::endl;
+
+            std::cerr << "Processing commits for project #" << current_project
+                      << std::endl;
+
+            processExistingData();
+            current_project = project_id;
+
+            std::cerr << "Reading project #" << project_id << std::endl;
+        }
+
+        // Process the data from the current row.
+        aggregateProjectInfo(project_id, path_id, commit_id);
     }
-
-    // Process the data from the current row.
-    aggregateProjectInfo(project_id, path_id, commit_id);
 }
--- src/timestamp-reader.cpp
+++ src/timestamp-reader.cpp
@@ -1,20 +1,21 @@
 #include <unordered_map>
 #include "timestamp-reader.h"
 
-using namespace dejavu;
+namespace dejavu {
 
-TimestampReader::TimestampReader(std::string const & path) : path(path) {}
+    TimestampReader::TimestampReader(std::string const &path) : path(path) {}
 
-void TimestampReader::read() {
-    parse(path);
-}
+    void TimestampReader::read() {
+        parse(path);
+    }
 
-void TimestampReader::row(std::vector<std::string> & row) {
-    unsigned int commit_id = atoi(row[0].c_str());
-    unsigned long timestamp = atol(row[1].c_str());
-    timestamps[commit_id] = timestamp;
-}
+    void TimestampReader::row(std::vector<std::string> &row) {
+        unsigned int commit_id = atoi(row[0].c_str());
+        unsigned long timestamp = atol(row[1].c_str());
+        timestamps[commit_id] = timestamp;
+    }
 
-std::unordered_map<unsigned int, unsigned long> const & TimestampReader::getTimestamps() {
-    return timestamps;
+    std::unordered_map<unsigned int, unsigned long> const &TimestampReader::getTimestamps() {
+        return timestamps;
+    }
 }
