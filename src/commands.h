33443e70c9ba0bb727431c6d3b0a31448cddb726
Konrad Siek <konrad.siek@gmail.com>
2019-04-17 14:50:09 +0200

    Extract projects from GHTorrent.

--- src/commands.h
+++ src/commands.h
@@ -19,4 +19,9 @@ namespace dejavu {
         This command is intended to run in a batch mode and as such does not provide for incremental analysis, or for caching the results. Each run will start fresh since the scenario is that when new projects are added to the corpus, they can be the new originals of any possible clone cluster.
      */
     void DetectFileClones(int argc, char * argv[]);
+
+    /** Reads the projects.csv file extracted form GHTorrent and extracts forked and non-forked, non-deleted, Javascript projects.
+     *
+     */
+    void ExtractJSProjects(int argc, char * argv[]);
 } // namespace dejavu
