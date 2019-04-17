14336284771d66aa1d2478f5db8b56bc4e086e04
peta <peta.maj82@gmail.com>
2019-04-17 17:22:54 +0200

    Fixed csv reader to work with escaped new lines.
    Fixes escaped new line followed immediately by a quote.

--- helpers/csv-reader.h
+++ helpers/csv-reader.h
@@ -94,14 +94,24 @@ namespace helpers {
                     while (line[i] != quote_) {
                         if (i == line.size() - 1 && eof())
                             throw std::ios_base::failure(STR("Unterminated quote, starting at line " + quoteStart));
+                        // check for escaped characters
                         if (line[i] == '\\') {
                             ++i;
-                            while (i == line.size() && ! eof()) {
-                                line = readLine();
-                                i = 0;
+                            // if the new line is escaped, we already have the escaped character
+                            if (i == line.size()) {
+                                while (i == line.size() && ! eof()) {
+                                    col += '\n';
+                                    line = readLine();
+                                    i = 0;
+                                }
+                                // otherwise just add the escaped character
+                            } else {
+                                col += line[i++];
                             }
+                        // normal character, just append
+                        } else {
+                            col += line[i++];
                         }
-                        col += line[i++];
                         while (i == line.size() && ! eof()) {
                             line = readLine();
                             i = 0;
